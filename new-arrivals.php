29f773839d621684c932d97c675a1dee3694980d
Senavirathna W.H.D.A <dulsharulzzz@gmail.com>
2024-02-12 15:31:38 +0530

    fwf

--- new-arrivals.php
+++ new-arrivals.php
@@ -66,61 +66,7 @@
         </div>
     </section>
 
-    <!-- Footer Section -->
-    <footer>
-        <div class="container">
-            <div class="grid">
-                <div class="footer-col">
-                    <h4>Company</h4>
-                    <ul>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Terms & Conditions</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>Support</h4>
-                    <ul>
-                        <li><a href="#">Faq</a></li>
-                        <li><a href="#">Accessibility</a></li>
-                        <li><a href="#">Customer Services</a></li>
-                        <li><a href="#">Return and Refund</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>Categories</h4>
-                    <ul>
-                        <li><a href="#">Men</a></li>
-                        <li><a href="#">Women</a></li>
-                        <li><a href="#">Kids</a></li>
-                        <li><a href="#">Accessories</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <img src="" alt="logo" class="footer-logo-img">
-                    <div class="contact">
-                        <a href="#"><i class="ri-mail-line"></i> email@mail.com</a>
-                        <a href="#"><i class="ri-phone-line"></i> +9422566445</a>
-                    </div>
-                    <div class="social-icon">
-                        <a href="#" title="facebook"> 
-                            <i class="ri-facebook-fill"></i>
-                        </a>
-                        <a href="#" title="instagram"> 
-                            <i class="ri-instagram-fill"></i>
-                        </a>
-                        <a href="#" title="twitter"> 
-                            <i class="ri-twitter-fill"></i>
-                        </a>
-                        <a href="#" title="linkedin"> 
-                            <i class="ri-linkedin-fill"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+   
     
 </body>
 </html>
